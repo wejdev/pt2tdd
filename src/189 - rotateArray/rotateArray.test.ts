e6ed170b1f63f978fb7239216c031e3c2d9ac9e3
billj <willard.johnson@eso.com>
2022-02-28 14:00:40 -0800

    Implement Rotate Array

---
+++ src/189 - rotateArray/rotateArray.test.ts
@@ -0,0 +1,17 @@
+import { rotateArray } from "./rotateArray";
+
+describe("test rotateArray function", () => {
+    test.each(
+        [
+            { input: [1, 2, 3, 4, 5, 6, 7], k: 3, expected: [5, 6, 7, 1, 2, 3, 4] },
+            { input: [-1, -100, 3, 99], k: 2, expected: [3, 99, -1, -100] },
+            { input: [1, 2], k: 5, expected: [2, 1] }
+
+        ])(
+            'rotateArray(%s) should alter input to be %s', (example: { input: number[], k: number, expected: number[] }) => {
+                let nums = example.input;
+                rotateArray(nums, example.k);
+                expect(nums).toStrictEqual(example.expected);
+            },
+        );
+});
