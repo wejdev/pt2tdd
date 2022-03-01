6c6ecb66165ee3f4a41d28a051d24094391757d3
wejdev <wejdev@gmail.com>
2022-02-28 23:31:18 -0800

    Implement reverse string in words 3

---
+++ src/557 - reverseStringInWords3/reverseStringInWords3.test.ts
@@ -0,0 +1,16 @@
+import { reverseStringInWords3 } from "./reverseStringInWords3";
+
+describe("test reverseStringInWords3 function", () => {
+    test.each(
+        [
+            { s: "a", expected: "a" },
+            { s: "Bill", expected: "lliB" },
+            { s: "Bill was here!", expected: "lliB saw !ereh" },
+
+            { s: "Let's take LeetCode contest", expected: "s'teL ekat edoCteeL tsetnoc" },
+            { s: "God Ding", expected: "doG gniD" },
+        ])(
+            'reverseString(%s) should alter input to be %s', (example: { s: string, expected: string }) => {
+                expect(reverseStringInWords3(example.s)).toBe(example.expected);
+            })
+});
---
+++ src/557 - reverseStringInWords3/reverseStringInWords3.ts
@@ -0,0 +1,41 @@
+function reverseWords(s: string): string {
+    return s
+        .split(" ")
+        .map(w => reverseString(w))
+        .join(" ");
+};
+
+function reverseString(str: string): string {
+    let s = str.split('');
+    let left = 0;
+    let right = s.length - 1;
+    while (left < right) {
+        [s[left], s[right]] = [s[right], s[left]];
+        left++;
+        right--;
+    }
+    return s.join('');
+};
+
+export { reverseWords as reverseStringInWords3 }
+
+/*
+557. Reverse Words in a String III
+Given a string s, reverse the order of characters in each word within a sentence while still
+preserving whitespace and initial word order.
+
+Example 1:
+Input: s = "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+
+Example 2:
+Input: s = "God Ding"
+Output: "doG gniD"
+
+Constraints:
+1 <= s.length <= 5 * 104
+s contains printable ASCII characters.
+s does not contain any leading or trailing spaces.
+There is at least one word in s.
+All the words in s are separated by a single space.
+*/
