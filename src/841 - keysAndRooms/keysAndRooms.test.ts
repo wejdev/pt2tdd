10ac9191367774655c1cc93565213c5534e61559
wejdev <wejdev@gmail.com>
2022-05-24 20:55:06 -0700

    841 - Add keys and rooms

---
+++ src/841 - keysAndRooms/keysAndRooms.test.ts
@@ -0,0 +1,30 @@
+import { keysAndRooms } from "./keysAndRooms";
+
+describe("test keysAndRooms function", () => {
+    test.each(
+        [
+            [
+                [[1], [1]],
+                true
+            ],
+            [
+                [[1], [1], [1]],
+                false
+            ],
+            [
+                [[1], [2], [3], []],
+                true
+            ],
+            [
+                [[1, 3], [3, 0, 1], [2], [0]],
+                false
+            ]
+        ])(
+            'keysAndRooms() should alter %j to be %s', (
+                rooms: number[][],
+                expected: boolean
+            ) => {
+            const result = keysAndRooms(rooms);
+            expect(result).toEqual(expected);
+        });
+});
