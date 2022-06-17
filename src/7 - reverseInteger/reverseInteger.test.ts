a83abe0cee4022f48c609fdff18ca03a5af51fe7
Bill Johnson <wejdev@gmail.com>
2022-06-17 05:12:36 +0000

    7 - Reverse Integer

---
+++ src/7 - reverseInteger/reverseInteger.test.ts
@@ -0,0 +1,18 @@
+import { reverseInteger } from "./reverseInteger";
+
+describe("test reverseInteger function", () => {
+    test.each(
+        [
+            [123, 321],
+            [-123, -321],
+            [120, 21],
+            [10000000, 1],
+            [9, 9],
+            [2147483647, 0]
+        ])(
+            'reverseInteger(%i) should return %i', (num, expected) => {
+                const result = reverseInteger(num);
+                expect(result).toStrictEqual(expected);
+            },
+        );
+});
---
+++ src/7 - reverseInteger/reverseInteger.ts
@@ -0,0 +1,45 @@
+function reverse(x: number): number {
+    const MAXINT = Math.pow(2, 31) - 1; // 2147483647
+    const MININT = Math.pow(-2, 31); // -2147483648
+
+    let rev = 0;
+    while (x != 0) {
+        let remainder = x % 10;
+        x = Math.trunc(x / 10);
+        rev = rev * 10 + remainder;
+    }
+    
+    return (rev < MININT || rev > MAXINT) ? 0 : rev;
+}
+
+function reverseUsingString(x: number): number {
+
+    return parseInt(x
+        .toString()
+        .split('')
+        .reverse()
+        .join());
+}
+
+export { reverse as reverseInteger }
+
+/*
+Given a signed 32-bit integer x, return x with its digits reversed.
+If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+Example 1:
+Input: x = 123
+Output: 321
+
+Example 2:
+Input: x = -123
+Output: -321
+
+Example 3:
+Input: x = 120
+Output: 21
+
+Constraints:
+-2^31 <= x <= 2^31 - 1
+*/
